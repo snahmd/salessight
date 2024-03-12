955c9b8ebf4f2c5ccfa6bb9a2447693b38f3f16f
snahmd <sanahmed7@gmail.com>
2024-03-12 14:55:53 +0100

    update purchases

--- stock/views.py
+++ stock/views.py
@@ -69,3 +69,27 @@ class PurchasesViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user) 
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        #Add Product Stock###
+        purchase = request.data
+        product = Product.objects.get(id=instance.product_id)
+        updated_quantity = purchase['quantity'] - instance.quantity
+        product.stock += updated_quantity
+        product.save()
+        ############################
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()    
